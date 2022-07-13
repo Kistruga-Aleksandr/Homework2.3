4ebf66a4a28624e8e49aba270f47184bec5906f2
Александр <a@kistruga.ru>
2022-07-13 07:24:46 +0400

    ДЗ ООП часть 2. Интерфейсы и полиморфизм Исправленая + Исправленая вторично

--- src/Main.java
+++ src/Main.java
@@ -13,18 +13,12 @@ public class Main {
         Bicycle bicycle = new Bicycle("Bicycle1", 2);
         Bicycle bicycle2 = new Bicycle("Bicycle2", 2);
 
-
-        car.check();
-        System.out.println();
-        car2.check();
-        System.out.println();
-        bicycle.check();
-        System.out.println();
-        bicycle2.check();
-        System.out.println();
-        truck.check();
-        System.out.println();
-        truck2.check();
+        ServiceStation.check(car);
+        ServiceStation.check(car2);
+        ServiceStation.check(bicycle);
+        ServiceStation.check(bicycle2);
+        ServiceStation.check(truck);
+        ServiceStation.check(truck2);
 
 
     }
